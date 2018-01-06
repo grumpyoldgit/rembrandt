e4b55b6c6aff982b4f22cd6d64ed711d5222e2ea
grumpoldgit <benjamin.winston@gmail.com>
2018-01-06 12:51:07 -0800

    Added test code for end-to-end round trip of data over mock serial port through http server

--- index.js
+++ index.js
@@ -34,7 +34,47 @@ if (opt.argv[0] == "test") {
 var config = require('config');
 var comport = config.get('Hardware.comport');
 
-// serial processing
+// Web server
+//
+// Serves static content as well as results from serial interface.
+
+var express = require('express')
+var app = express()
+
+// https://expressjs.com/en/guide/routing.html
+
+app.get('/ping', function (req, res) {
+  res.send('pong')
+})
+
+app.post('/ping', function (req, res) {
+  res.send('pong')
+})
+
+app.get('/status', function(req, res) {
+  res.send(JSON.stringify({credits: credits, pressed: presses}))
+  presses = []
+})
+
+app.post('/use_a_credit', function(req, res) {
+  res.send(--credits)
+})
+
+app.use('/static', express.static('static'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// Launch chrome
+//
+// Launches chrome in kiosk mode to open the app served over /static
+
+// todo
+
+
+// Serial processing
+//
+// Reads serial data from the serial port, decoding it so that the data
+// is available over the web application started above
 
 var SerialPort = require(test ? "serialport/test" : "serialport")
 
@@ -52,13 +92,52 @@ port.on('open', () => {
 
 if (test) {
   port.on('open', () => {
-    port.binding.emitData(Buffer.from(buttons["up"].data));
+    var readline = require('readline')
+    readline.emitKeypressEvents(process.stdin)
+    process.stdin.setRawMode(true)
+
+    var keymap = {'w':"up", 'a':"left", 's':"down", 'd':"right", 'o':"ok", 'c':"cancel", '$':"coin"}
+
+    console.log("in test mode, mock port open. Press " + Object.keys(keymap).toString() + " or q to quit.")
+
+    process.stdin.on('keypress', function (str, key) {
+      if (key.sequence in keymap) {
+        port.binding.emitData(Buffer.from(buttons[keymap[key.sequence]].data))
+      }
+      
+      if (key.name == 'q') {
+        process.exit(0)
+      }
+
+      const http = require('http')
+ 
+      http.get('http://127.0.0.1:3000/status', (resp) => {
+        let data = ''
+ 
+        resp.on('data', (chunk) => {
+         data += chunk
+        })
+ 
+        resp.on('end', () => {
+          console.log(data)
+          console.log("credits received via HTTP response: " + JSON.parse(data).credits)
+          console.log("pressed received via HTTP response: " + JSON.parse(data).pressed)
+        })
+      })
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    })
   })
 }
 
 function pressed(button) {
   console.log(button + " pressed")
-  presses.push(button);
+  if (button == "coin") {
+    credits++;
+  } else {
+    presses.push(button);
+  }
 }
 
 var buttons = {
@@ -96,39 +175,4 @@ function decode(incoming) {
 port.on('data', function(incoming) { // receives node Buffer
   decode(incoming)
 })
-
-
-// Web server
-//
-// Serves static content as well as results from serial interface.
-
-var express = require('express')
-var app = express()
-
-// https://expressjs.com/en/guide/routing.html
-
-app.get('/ping', function (req, res) {
-  res.send('pong')
-})
-
-app.post('/ping', function (req, res) {
-  res.send('pong')
-})
-
-app.get('/status', function(req, res) {
-  res.send(JSON.stringify({credits: credits, pressed: presses}))
-  presses = []
-})
-
-app.post('/use_a_credit', function(req, res) {
-  res.send(--credits)
-})
-
-app.use('/static', express.static('static'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-// Launch chrome
-//
-// Launches chrome in kiosk mode and keeps it open.
 
